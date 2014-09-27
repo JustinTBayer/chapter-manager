0a48ed1dd87a04f56a44aa98541ece49b271a6b8
Dale <race.dale@gmail.com>
2014-09-27 11:10:27 -0500

    Organized new code better

--- mysql_access.php
+++ mysql_access.php
@@ -4,7 +4,7 @@ $DBServer = 'mysql.truman.edu';
 $DBUser   = 'apo';
 $DBPass   = 'glueallE17';
 $DBName   = 'apo';
-
+// really bad way
 $db = mysql_connect("mysql.truman.edu", "apo", "glueallE17");
 if (!$db) {
         print "Error - Could not connect to mysql";
@@ -17,7 +17,7 @@ if (!$er) {
         exit;
 }
 // MYSQLI connection http://codular.com/php-mysqli
-/*
+/* Object oriented way (good way)
 $conn = new mysqli($DBServer, $DBUser, $DBPass, $DBName);
 
 // check connection
@@ -25,25 +25,11 @@ if ($conn->connect_error) {
   trigger_error('Database connection failed: '  . $conn->connect_error, E_USER_ERROR);
 }
 
-
+// procedural way (bad way)
 $conn = mysqli_connect($DBServer, $DBUser, $DBPass, $DBName);
 // check connection
 if (mysqli_connect_errno()) {
   trigger_error('Database connection failed: '  . mysqli_connect_error(), E_USER_ERROR);
-}
-
-$db_conn = mysqli_connect("$DBServer", "$DBUser", "$DBPass", "$DBName");
-
-// Evaluate the connection
-if (mysqli_connect_errno())
-{
-    echo mysqli_connect_error();
-    exit();
-}
-
-else
-{
-    echo "Successful database connection, happy coding!!!";
 }
 */
 ?>
