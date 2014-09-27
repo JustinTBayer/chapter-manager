5a79d20b568368bbd7e08450b3d8bfe49800c4a1
Dale <race.dale@gmail.com>
2014-09-27 11:08:33 -0500

    Added mysqli connect
    in the future it will be needed to switch to mysqli since mysql is
    deprecated

--- mysql_access.php
+++ mysql_access.php
@@ -1,14 +1,49 @@
 <?php
+error_reporting(-1);
+$DBServer = 'mysql.truman.edu';
+$DBUser   = 'apo';
+$DBPass   = 'glueallE17';
+$DBName   = 'apo';
 
-$db = mysql_connect("mysql.truman.edu", "apo", "glueallE17"); 
-if (!$db) { 
-    	print "Error - Could not connect to mysql"; 
-    	exit; 
-} 
-
-$er = mysql_select_db("apo"); 
-if (!$er) { 
-    	print "Error - Could not select database"; 
-    	exit; 
+$db = mysql_connect("mysql.truman.edu", "apo", "glueallE17");
+if (!$db) {
+        print "Error - Could not connect to mysql";
+        exit;
 }
+
+$er = mysql_select_db("apo");
+if (!$er) {
+        print "Error - Could not select database";
+        exit;
+}
+// MYSQLI connection http://codular.com/php-mysqli
+/*
+$conn = new mysqli($DBServer, $DBUser, $DBPass, $DBName);
+
+// check connection
+if ($conn->connect_error) {
+  trigger_error('Database connection failed: '  . $conn->connect_error, E_USER_ERROR);
+}
+
+
+$conn = mysqli_connect($DBServer, $DBUser, $DBPass, $DBName);
+// check connection
+if (mysqli_connect_errno()) {
+  trigger_error('Database connection failed: '  . mysqli_connect_error(), E_USER_ERROR);
+}
+
+$db_conn = mysqli_connect("$DBServer", "$DBUser", "$DBPass", "$DBName");
+
+// Evaluate the connection
+if (mysqli_connect_errno())
+{
+    echo mysqli_connect_error();
+    exit();
+}
+
+else
+{
+    echo "Successful database connection, happy coding!!!";
+}
+*/
 ?>
