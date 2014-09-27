4abb6ad6dffa8928dde6cf0628ffe0b51ac4d8a0
Dale <race.dale@gmail.com>
2014-09-27 11:50:33 -0500

    Example ways of using mysqli instead of mysql

--- test.php
+++ test.php
@@ -1,18 +1,53 @@
 <?php
-function curPageURL() {
- $pageURL = 'http';
- if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
- $pageURL .= "://";
- if ($_SERVER["SERVER_PORT"] != "80") {
-  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
- } else {
-  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
- }
- return $pageURL;
+// MYSQLI connection http://codular.com/php-mysqli
+error_reporting(-1);
+$DBServer = 'mysql.truman.edu';
+$DBUser   = 'apo';
+$DBPass   = 'glueallE17';
+$DBName   = 'apo';
+
+// Object oriented way (good way)
+    $db = new mysqli($DBServer, $DBUser, $DBPass, $DBName);
+
+// check connection
+    if ($db->connect_error) {
+      trigger_error('Database connection failed: '  . $db->connect_error, E_USER_ERROR);
+    }
+//more readable way of doing sql
+$sql = <<<SQL
+    SELECT *
+    FROM `apo`.`contact_information`
+    WHERE id = 419
+SQL;
+
+if(!$result = $db->query($sql)){
+    die('There was an error running the query [' . $db->error . ']');
+}
+while($row = $result->fetch_assoc()){
+    echo $row['firstname'] . '<br />';
 }
 
-$curPageURLResult = curPageURL();
+/*
 
-echo $curPageURLResult;
-echo "<meta http-equiv=\"refresh\" content=\"0;url=http://apo.truman.edu/bootstrap/index.php?u=$curPageURLResult\"/>";
+// procedural way (bad way)
+$conn = mysqli_connect($DBServer, $DBUser, $DBPass, $DBName);
+// check connection
+if (mysqli_connect_errno()) {
+  trigger_error('Database connection failed: '  . mysqli_connect_error(), E_USER_ERROR);
+}
+
+// really bad way of connect to mysql
+    $db = mysql_connect("mysql.truman.edu", "apo", "glueallE17");
+    if (!$db) {
+            print "Error - Could not connect to mysql";
+            exit;
+    }
+
+    $er = mysql_select_db("apo");
+    if (!$er) {
+            print "Error - Could not select database";
+            exit;
+    }
+
+*/
 ?>
